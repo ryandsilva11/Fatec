1d10fdd410355391d2d32e23d9874803e6b9fa80
Ryan <ryandsilva31.19@gmail.com>
2026-08-31 11:19:06 -0300

    docs(matcom): Adiciona um documento com a continuacao do codigo das unioes

--- Semestre2/matcom/atividades/fragoso.c
+++ Semestre2/matcom/atividades/fragoso.c
@@ -1,38 +1,74 @@
-#include <conio.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+#include <conio.h>
 
 int main() {
-  system("cls");
-  int a[6], u[12], usr[12], i, c, j, chave;
-  printf("Este eh o programa da Uniao.");
-  system("cls");
+  int a[6], b[6], u[12], usr[12], i, j, c, chave;
+
+  printf("\n\nEste e o programa da Uniao");
+
+  for (i = 1; i <= 5; i++)
+
+  {
+
+    printf("\nDigite o %d elemento de A: ", i);
+
+    scanf("%d", &a[i]);
+  }
+
+  for (i = 1; i <= 5; i++)
+
+  {
+
+    printf("\nDigite o %d elemento de B: ", i);
+
+    scanf("%d", &b[i]);
+  }
+
+  for (i = 1; i <= 5; i++)
+
+  {
+
+    u[i] = a[i];
+
+    u[i + 5] = b[i];
+  }
+
+  for (i = 1; i <= 10; i++)
+
+    printf("\nUnião [%d] = %d", i, u[i]);
 
   c = 1;
+
   chave = 0;
+
   usr[1] = u[1];
 
-  for (i = 2; i <= 10; i++) {
+  for (i = 2; i <= 10; i++)
 
-    for (j = 1; j <= c; j++) {
+  {
 
-      if (u[i] == usr[j]) {
+    for (j = 1; j <= c; j++)
+
+      if (u[i] == usr[j])
         chave = 1;
-      }
-    }
 
-    if (chave == 0) {
-      c++;
+    if (chave == 0)
+
+    {
+      c = c + 1;
+
       usr[c] = u[i];
     }
 
     chave = 0;
-
-    printf("\nUniao sem os repetidos:");
-    for (i = 1; i <= c; i++) {
-      printf("\n %d", usr[i]);
-    }
-
-    return 0;
   }
+
+  printf("\nUniao sem os repetidos");
+
+  for (i = 1; i <= c; i++)
+
+    printf("\n %d", usr[i]);
+
+  return 0;
 }
---
+++ Semestre2/matcom/atividades/fragoso2.c
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int a[6], i, j, c = 0;
+
+  for (c = 1; c <= 5; c++) {
+    if (c == 1) {
+      scaf("%d", a[c]);
+    }
+  }
+}
