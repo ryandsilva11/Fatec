56343793aad8725ab75f15e10266bd4198b94cbf
Ryan <ryandsilva31.19@gmail.com>
2026-09-02 11:07:02 -0300

    feat(desweb/forms): Adiciona um botão de contador

--- Semestre2/desweb/React/forms/src/App.jsx
+++ Semestre2/desweb/React/forms/src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react'
+
 function App() {
+  const [cont, setCont] = useState(0)
+
+  function alterarContador() {
+    setCont(cont + 1)
+  }
 
   return (
     <>
-
+      <div>
+        <h2>{cont}</h2>
+        <button onClick={alterarContador}>Adicionar</button>
+      </div>
     </>
   )
 }
+
+export default App
