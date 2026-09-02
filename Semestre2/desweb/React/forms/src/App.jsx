9a218f0b76c950087bf35e2add3b346b436c218b
Ryan <ryandsilva31.19@gmail.com>
2026-09-02 11:15:32 -0300

    feat(desweb/forms): Remove a funcao alterarContador

--- Semestre2/desweb/React/forms/src/App.jsx
+++ Semestre2/desweb/React/forms/src/App.jsx
@@ -3,15 +3,13 @@ import { useState } from 'react'
 function App() {
   const [cont, setCont] = useState(0)
 
-  function alterarContador() {
-    setCont(cont + 1)
-  }
-
   return (
     <>
       <div>
         <h2>{cont}</h2>
-        <button onClick={alterarContador}>Adicionar</button>
+        <button onClick={() => setCont(cont + 1)}>Somar</button>
+        <button onClick={() => setCont(cont - 1)}>Subtrair</button>
+        <button onClick={() => setCont(0)}>Zerar</button>
       </div>
     </>
   )
