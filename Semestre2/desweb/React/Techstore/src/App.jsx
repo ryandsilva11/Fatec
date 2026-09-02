54429e511c71781b5d6d57355ea3102b679a2c80
Ryan <ryandsilva31.19@gmail.com>
2026-09-02 12:32:12 -0300

    feat(desweb/techstore): Cria Header, Hero e Category (jsx)

---
+++ Semestre2/desweb/React/Techstore/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Componentes/Header";
+import Hero from "./Componentes/Hero";
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ Semestre2/desweb/React/Techstore/src/Componentes/Category.jsx
@@ -0,0 +1,6 @@
+function Category(props) {
+    return (
+        <>
+        </>
+    )
+}
